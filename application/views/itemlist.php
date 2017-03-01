16427501bdbec0bf7aaa72582eccf463828a437b
ariel0058 <ariel0058@hotmail.com>
2017-03-01 10:57:24 -0800

    job 6 completed

--- application/views/itemlist.php
+++ application/views/itemlist.php
@@ -1,13 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-   <h3>Tasks by Category</h3>
-
-    <table class="table">
-        <tr>
-                <th>Id</th>
-                <th>Task</th>
-                <th>Status</th>
-        </tr>
-        {display_tasks}
-	</table>
-    </html>
+{pagination}
+<table class="table">
+    <tr>
+        <th>Id</th>
+        <th>Task</th>
+        <th>Status</th>
+    </tr>
+    {display_tasks}
+</table>
