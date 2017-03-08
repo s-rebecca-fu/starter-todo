4e29c8ebd1dd7de87451cf79e373a087c916b702
s-rebecca-fu <rebeccafu1991@gmail.com>
2017-03-07 22:50:30 -0500

    job10 finished

--- application/controllers/Views.php
+++ application/controllers/Views.php
@@ -35,6 +35,8 @@ class Views extends Application
         }
         // and then pass them on
         $parms = ['display_tasks' => $converted];
+		$role = $this->session->userdata('userrole');
+		$parms['completer'] = ($role == ROLE_OWNER) ? '/views/complete' : '#';
         return $this->parser->parse('by_priority', $parms, true);
 
     }
@@ -44,6 +46,24 @@ class Views extends Application
         $parms = ['display_tasks' => $this->tasks->getCategorizedTasks()];
         return $this->parser->parse('by_category', $parms, true);
     }
+	
+	// complete flagged items
+	function complete() {
+		$role = $this->session->userdata('userrole');
+		if ($role != ROLE_OWNER) redirect('/work');
+		
+		// loop over the post fields, looking for flagged tasks
+		foreach($this->input->post() as $key=>$value) {
+			if (substr($key,0,4) == 'task') {
+				// find the associated task
+				$taskid = substr($key,4);
+				$task = $this->tasks->get($taskid);
+				$task->status = 2; // complete
+				$this->tasks->update($task);
+			}
+		}
+		$this->index();
+	}
 	
 }
 
